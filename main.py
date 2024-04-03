99ea582614d76d21f3a5861bb0a897d15318fc94
awhiot <timaparshikov@gmail.com>
2024-04-03 15:05:34 +0300

    first_serv_ver

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+import flask
+import json
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def hello():
+    return "Hello, world!"
+
+
+@app.route("/users", methods=["GET", "POST"])
+def users():
+    print("users endpoint reached...")
+    if request.method == "GET":
+        with open("users.json", 'r') as f:
+            data = json.load(f)
+            data.append({
+                "username": "user4",
+                "pets": ["hamster"]
+            })
+
+            return flask.jsonify(data)
+
+    if request.method == "POST":
+        received_data = request.get_json()
+        print(f"received data: {received_data}")
+        message = received_data['data']
+        return_data = {
+            "status": "success",
+            "message": f"received: {message}"
+        }
+        return flask.Response(response=json.dumps(return_data), status=201)
+
+
+if __name__ == "__main__":
+    app.run("localhost", 1337)
