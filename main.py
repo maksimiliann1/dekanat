72b541d635c78b021b9bcd66c8e3efac2566d21d
Mikael Nazaryan <mikaelnaz@mail.ru>
2024-04-07 23:46:22 +0300

    Добавлена функция authentication

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 import flask
 import json
 from flask_cors import CORS
@@ -35,20 +35,37 @@ def users():
             received_data = request.get_json()
             login = received_data['login']
             password = received_data["password"]
-            # mode = received_data["mode"]
 
-            account = accounts.query.filter_by(login=login, password=password).first()
-            if account is not None:
-                return flask.Response(response=json.dumps({"status": "success",},), status=200)
+            account_id, mode = authentication(login, password)
+
+            if account_id is not None and mode is not None:
+                response_data = {
+                    "status": "success",
+                    "id": account_id,
+                    "mode": mode
+                }
+                print(response_data)
+                return flask.Response(response=json.dumps(response_data, ensure_ascii=False), status=200, mimetype='application/json')
             else:
-                return flask.Response(response=json.dumps({"status": "error"}), status=401)
+                return flask.Response(response=json.dumps({"status": "error"}), status=401, mimetype='application/json')
+
     except Exception as e:
         traceback.print_exc()
         return flask.Response(response=json.dumps({"status": "error", "message": str(e)}), status=500)
 
 
 
+def authentication(login, password):
+    try:
+        account = accounts.query.filter_by(login=login, password=password).first()
 
+        if account is not None:
+            return account.id, account.mode
+        else:
+            return None, None
+    except Exception as e:
+        traceback.print_exc()
+        return None, None
 
 
 
@@ -56,5 +73,15 @@ if __name__ == "__main__":
     app.run("localhost", 1337)     
 
 
+
     # def authentication(догин и пароль в джейсон формате):
     # она ишет запись в которой есть логин и пароль и возвращает мод и ид
+
+   #         account = accounts.query.filter_by(login=login, password=password).first()
+    #         if account is not None:
+    #             return flask.Response(response=json.dumps({"status": "success",},), status=200)
+    #         else:
+    #             return flask.Response(response=json.dumps({"status": "error"}), status=401)
+    # except Exception as e:
+    #     traceback.print_exc()
+    #     return flask.Response(response=json.dumps({"status": "error", "message": str(e)}), status=500)
