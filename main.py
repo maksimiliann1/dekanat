d79eb57a73d3f5c99a98891ea617c6872f25a23c
Mikael Nazaryan <mikaelnaz@mail.ru>
2024-04-07 23:18:00 +0300

    Исправлена ошибка 500 и подключена БД

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import psycopg2
-from models import db, departments
+from models import db, departments, accounts
+import traceback
 
 app = Flask(__name__)
 CORS(app)
@@ -15,37 +16,45 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 
-# @app.route("/")
-# def hello():
-#     return "Hello, world!"
-
 
 @app.route("/", methods=["GET", "POST"])
 def users():
     print("users endpoint reached...")
-    if request.method == "GET":
-        with open("users.json", 'r') as f:
-            data = json.load(f)
-            data.append({
-                "username": "user4",
-                "pets": ["hamster"]
-            })
+    try:
+        if request.method == "GET":
+            with open("users.json", 'r') as f:
+                data = json.load(f)
+                data.append({
+                    "username": "user4",
+                    "pets": ["hamster"]
+                })
 
-            return flask.jsonify(data)
+                return flask.jsonify(data)
 
-    if request.method == "POST":
-        received_data = request.get_json()
-        print(f"received data: {received_data}")
-        if received_data['login'] == 'maxaufov':
-            mode = 'admin'
-        else:
-            mode = 'viewer'
-        return_data = {
-            "status": "success",
-            "mode": f"{mode}"
-        }
-        return flask.Response(response=json.dumps(return_data), status=201)
+        if request.method == "POST":
+            received_data = request.get_json()
+            login = received_data['login']
+            password = received_data["password"]
+            # mode = received_data["mode"]
+
+            account = accounts.query.filter_by(login=login, password=password).first()
+            if account is not None:
+                return flask.Response(response=json.dumps({"status": "success",},), status=200)
+            else:
+                return flask.Response(response=json.dumps({"status": "error"}), status=401)
+    except Exception as e:
+        traceback.print_exc()
+        return flask.Response(response=json.dumps({"status": "error", "message": str(e)}), status=500)
+
+
+
+
+
 
 
 if __name__ == "__main__":
-    app.run("localhost", 1337)
+    app.run("localhost", 1337)     
+
+
+    # def authentication(догин и пароль в джейсон формате):
+    # она ишет запись в которой есть логин и пароль и возвращает мод и ид
--- models.py
+++ models.py
@@ -61,6 +61,7 @@ class accounts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
-    students_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
+    mode = db.Column(db.String(50), nullable=False)
     def __repr__(self):
         return f"Accounts(name='{self.students_id}')"
+    
