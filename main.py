4ca10f4038fc5753fb6089049892b875a42f405a
awhiot <timaparshikov@gmail.com>
2024-05-11 17:34:37 +0300

    added stages and getting modules

--- main.py
+++ main.py
@@ -33,30 +33,61 @@ def users():
 
         if request.method == "POST":
             received_data = request.get_json()
-            login = received_data['login']
-            password = received_data["password"]
+            stage = get_stage(received_data)
+            if stage == '1':
+                login = received_data['login']
+                password = received_data["password"]
 
-            account_id, mode = authentication(received_data['login'], received_data["password"])
+                account_id, mode = authentication(received_data['login'], received_data["password"])
 
-            if account_id is not None and mode is not None:
-                subjs = []
-                if mode == 'Преподаватель':
-                    subjs = [subj[0] for subj in marks.query.with_entities(marks.subject_name).distinct()]
+                if account_id is not None and mode is not None:
+                    subjs = []
+                    if mode == 'Преподаватель':
+                        subjs = [subj[0] for subj in marks.query.with_entities(marks.subject_name).distinct()]
 
-                response_data = {
-                    "status": "success",
-                    "subjects": subjs,
-                    "id": account_id,
-                    "mode": mode
-                }
-                print(response_data)
-                return flask.Response(response=json.dumps(response_data, ensure_ascii=False), status=200, mimetype='application/json')
-            else:
-                return flask.Response(response=json.dumps({"status": "error"}), status=401, mimetype='application/json')
+                    response_data = {
+                        "status": "success",
+                        "subjects": subjs,
+                        "id": account_id,
+                        "mode": mode
+                    }
+                    print(response_data)
+                    return flask.Response(response=json.dumps(response_data, ensure_ascii=False), status=200, mimetype='application/json')
+                else:
+                    return flask.Response(response=json.dumps({"status": "error"}), status=401, mimetype='application/json')
+            elif stage == '2':
+                subject, group = received_data['subject'], received_data['gruop']
+                if subject is not None and group is not None:
+                    ents = []
+                    ents_query = marks.query.filter_by(subject_name=subject, group_name=group)
+                    for ent in ents_query:
+                        ents.append({
+                            'id': ent.id,
+                            'student_id': ent.student_id,
+                            'group_name': ent.group_name,
+                            'subject_name': ent.subject_name,
+                            'module_1': ent.module_1,
+                            'module_2': ent.module_2,
+                            'last_mark': ent.last_mark
+                        })
+                    response_data = {
+                        "status": "success",
+                        "entities": ents,
+                        }
+                    print(response_data)
+                    return flask.Response(response=json.dumps(response_data, ensure_ascii=False), status=200, mimetype='application/json')
+                else:
+                    return flask.Response(response=json.dumps({"status": "error"}), status=401, mimetype='application/json')
+
+
 
     except Exception as e:
         traceback.print_exc()
         return flask.Response(response=json.dumps({"status": "error", "message": str(e)}), status=500)
+
+
+def get_stage(f):
+    return f['stage']
 
 
 
@@ -75,18 +106,3 @@ def authentication(login, password):
 
 if __name__ == "__main__":
     app.run("localhost", 1337)     
-  
-
-
-
-    # def authentication(догин и пароль в джейсон формате):
-    # она ишет запись в которой есть логин и пароль и возвращает мод и ид
-
-   #         account = accounts.query.filter_by(login=login, password=password).first()
-    #         if account is not None:
-    #             return flask.Response(response=json.dumps({"status": "success",},), status=200)
-    #         else:
-    #             return flask.Response(response=json.dumps({"status": "error"}), status=401)
-    # except Exception as e:
-    #     traceback.print_exc()
-    #     return flask.Response(response=json.dumps({"status": "error", "message": str(e)}), status=500)
